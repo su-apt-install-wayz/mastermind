2911b5a9504a0491de0b947310f0bebcf4b97baf
su apt install wayz <104500669+su-apt-install-wayz@users.noreply.github.com>
2023-03-02 17:11:22 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+let ordreCouleur = 0;
+let ligneFini = 0;
+
+let couleur_random = [];
+const ligneColor = [];
+    
+for (let i = 0; i < 4; i++) {
+    color = Math.floor(Math.random() * 5) + 1;
+
+    switch(color) {
+        case 1:
+            colorBulle = "red";
+            break;
+
+        case 2:
+            colorBulle = "yellow";
+            break;
+               
+        case 3:
+            colorBulle = "green";
+            break;
+             
+        case 4:
+            colorBulle = "blue";
+            break;
+           
+        case 5:
+            colorBulle = "white";
+            break;
+    }
+    couleur_random.push(colorBulle);
+}
+
+console.log(couleur_random);
+
+
+
+function getColors(color) {
+    const bulleColor = document.getElementsByClassName('bulle');
+    console.log(color);
+    let nbrBulle = bulleColor.length;
+    bulleColor[ordreCouleur].style.backgroundColor = color;
+
+    ordreCouleur++;
+    ligneFini++;
+
+    ligneColor.push(color);
+
+    if (nbrBulle == ordreCouleur) {
+        document.body.innerHTML += "<div class='win'><h1>Game Over</h1><br>Rejouer</div>";
+    }
+
+    if (ligneFini == 4) {
+        if (JSON.stringify(couleur_random) === JSON.stringify(ligneColor)) {
+            document.body.innerHTML += "<div class='win'><h1>Félicitations !!</h1><a href'#'>Rejouer</a></div>";
+        }
+        else {
+            ligneFini = 0;
+            ligneColor.splice(color);
+        }
+    }
+}
