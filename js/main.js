954018acfdb95fe2bfd696c73384be2080190520
su apt install wayz <wayzzy59@gmail.com>
2023-03-05 14:07:31 +0100

    com

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
+// variable utile pour l'ordre des bulles à remplir
 let ordreCouleur = 0;
+// variable utile pour l'ordre des indices sur le côté
 let ordreToast = 0;
+// variable 0 ou 1 pour une ligne finie
 let ligneFini = 0;
 
+// tableaux vides pour les couleurs et pour une ligne de couleur afin de les comparer
 let couleur_random = [];
 const ligneColor = [];
 
-
+// fonction qui permet de récupérer la difficulté et la stocker, puis changer de page
 function getDifficulte(difficulte) {
     localStorage.setItem('laDifficulte', difficulte);
     window.location.href = "jeu.html";
 }
 
+// fonction qui permet de créer les lignes en fonction de la difficulté
 function ligneBoucle() {
     const game = document.getElementById('game');
 
+    // récupère la difficulté stockée
     let laDifficulte = localStorage.getItem("laDifficulte");
-    console.log(laDifficulte);
 
+    //définit le nombre de lignes en fonction de la difficulté
     switch(laDifficulte) {
         case 'facile':
             nbLigne = 8;
@@ -31,11 +37,13 @@ function ligneBoucle() {
             break;
     }
 
+    // crée les lignes par rapport au nombre de lignes
     for (var i = 1; i <= nbLigne; i++) {
         game.insertAdjacentHTML("afterbegin","<div class='ligne'><div class='bulle'></div><div class='bulle'></div><div class='bulle'></div><div class='bulle'></div><div class='toast' id='toast'>2</div></div>");
     }
 }
     
+// choisit aléatoirement 4 chiffres, ceux-ci correspondent à un code pour avoir la couleur
 for (let i = 0; i < 4; i++) {
     color = Math.floor(Math.random() * 5) + 1;
 
@@ -60,10 +68,12 @@ for (let i = 0; i < 4; i++) {
             colorBulle = "white";
             break;
     }
+    // met les 4 couleurs choisies aléatoirement dans le tableau
     couleur_random.push(colorBulle);
 }
 
 console.log(couleur_random);
+
 
 function getColors(color) {
     const bulleColor = document.getElementsByClassName('bulle');
