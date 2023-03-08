77be0bee8f9a68e05a8f958bc4f5e1fa8b330129
su apt install wayz <wayzzy59@gmail.com>
2023-03-08 15:39:32 +0100

    aaa

--- js/main.js
+++ js/main.js
@@ -98,10 +98,32 @@ function getColors(color) {
             document.body.innerHTML += "<div class='win'><h1>Félicitations !!</h1><img class='gif' src='./assets/win.gif'><a onclick='reload()'>Rejouer</a></div>";
         }
         else {
-            ligneFini = 0;
-            ligneColor.splice(color);
+
             toast[ordreToast].classList.add('score_on');
             ordreToast++;
+
+            let bonPlacement = [];
+
+            for(let i = 0; i < couleur_random.length; i++){
+            if(couleur_random[i] === ligneColor[i]){
+                bonPlacement.push(couleur_random[i]);
+            } else {
+                bonPlacement.push(color);
+            }
+            }
+
+            // mettre à jour les bulles d'indice en conséquence
+            let bullesIndice = document.getElementsByClassName('bulle_indice');
+
+            for(let i = 0; i < bullesIndice.length; i++){
+                if(bonPlacement[i]==couleur_random[i]) {
+                    bullesIndice[i].style.backgroundColor = 'red';
+                } else {
+                    bullesIndice[i].style.backgroundColor = 'white';
+                }
+            }
+            ligneFini = 0;
+            ligneColor.splice(color);
         }
     }
 }
--- js/test.js
+++ js/test.js
@@ -1,24 +1,38 @@
-let tableau1 = ['red', 'blue', 'green'];
-let tableau2 = ['red', 'green', 'green'];
-
-// comparer les deux tableaux et identifier les éléments bien placés
 let bonPlacement = [];
-
-for(let i = 0; i < tableau1.length; i++){
-  if(tableau1[i] === tableau2[i]){
-    bonPlacement.push(true);
-  } else {
-    bonPlacement.push(false);
-  }
+for(let i = 0; i < couleur_random.length; i++){
+    if(couleur_random[i] === ligneColor[i]){
+        bonPlacement.push(couleur_random[i]);
+    } else {
+        bonPlacement.push(color);
+    }
 }
 
 // mettre à jour les bulles d'indice en conséquence
-let bullesIndice = document.querySelectorAll('.bulle_indice');
+let bullesIndice = document.getElementsByClassName('bulle_indice');
+let ligneFini = true; // Initialiser la variable ici
 
 for(let i = 0; i < bullesIndice.length; i++){
-  if(bonPlacement[i]) {
-    bullesIndice[i].style.backgroundColor = 'red';
-  } else {
-    bullesIndice[i].style.backgroundColor = 'white';
-  }
+    const isBonPlacement = (bonPlacement[i]===couleur_random[i]);
+    
+    if(isBonPlacement) {
+        bullesIndice[i].style.backgroundColor = 'red';
+    } else {
+        bullesIndice[i].style.backgroundColor = 'white';
+        
+        // Si une seule des bulles d'indice est blanche, la ligne n'est pas terminée
+        ligneFini = false;
+    }
+}
+
+if (ligneFini) {
+    // Code à exécuter si la ligne est terminée
+    console.log('Ligne terminée!');
+    
+    // Réinitialisation des variables
+    ligneFini = false;
+    ligneColor = []; // Attention: la variable s'appelle ligneColor, donc pas besoin de l'argument color dans splice() 
+    colorIndex = 0;
+} else {
+    // Code à exécuter si la ligne n'est pas encore terminée
+    console.log('Ligne non terminée');
 }
