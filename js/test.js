8376a036f1890c6325b45ec0f741e54aeb470ed3
su apt install wayz <wayzzy59@gmail.com>
2023-03-08 14:38:31 +0100

    a

--- js/test.js
+++ js/test.js
@@ -1,54 +1,24 @@
-let tableau1 = ["a", "b", "c", "d"]
-let tableau2 = ["a", "b", "3", "4"]
+let tableau1 = ['red', 'blue', 'green'];
+let tableau2 = ['red', 'green', 'green'];
 
-let nbPlace = 0
+// comparer les deux tableaux et identifier les éléments bien placés
+let bonPlacement = [];
 
-if (tableau1[0] === tableau2[0]) {
-    console.log('ok premier element')
-    nbPlace++
-}
-
-if (tableau1[1] === tableau2[1]) {
-    console.log('ok second element')
-    nbPlace++
-}
-
-if (tableau1[2] === tableau2[2]) {
-    console.log('ok 3element')
-    nbPlace++
-}
-
-if (tableau1[3] === tableau2[3]) {
-    console.log('ok 4element')
-    nbPlace++
-}
-
-console.log('nb : '+nbPlace)
-
-
-switch(nbPlace) {
-    
-}
-
-function compareTableaux(tableau1, tableau2) {
-    // Vérifiez que les deux tableaux ont la même longueur
-    if (tableau1.length !== tableau2.length) {
-      return false;
-    }
-  
-    // Parcourez chaque élément et comparez-les à chaque index
-    for (let i = 0; i < tableau1.length; i++) {
-      if (tableau1[i] !== tableau2[i]) {
-        return false;
-      }
-    }
-  
-    // Si tous les éléments correspondent, retourner vrai
-    return true;
+for(let i = 0; i < tableau1.length; i++){
+  if(tableau1[i] === tableau2[i]){
+    bonPlacement.push(true);
+  } else {
+    bonPlacement.push(false);
   }
-  
-  // Exemple d'utilisation :
-  const tableauA = [1, 2, 3];
-  const tableauB = [1, 2, 4];
-  console.log(compareTableaux(tableauA, tableauB)); // Renvoie false
-  
+}
+
+// mettre à jour les bulles d'indice en conséquence
+let bullesIndice = document.querySelectorAll('.bulle_indice');
+
+for(let i = 0; i < bullesIndice.length; i++){
+  if(bonPlacement[i]) {
+    bullesIndice[i].style.backgroundColor = 'red';
+  } else {
+    bullesIndice[i].style.backgroundColor = 'white';
+  }
+}
