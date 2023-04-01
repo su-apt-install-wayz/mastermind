1c97cd2d2959c3eecaf40310d3d54fb5da7cd9e5
su apt install wayz <wayzzy59@gmail.com>
2023-04-01 11:19:21 +0200

    comments

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@ let statut = "";
 let date = new Date(); // crée un objet Date avec la date actuelle
 let jour = date.getDate(); // retourne le jour du mois (1-31)
 let mois = date.getMonth()+1; // retourne le mois (1-12)
-let annee = date.getFullYear();
+let annee = date.getFullYear(); // retourne l'année
 date_score = jour+"/"+mois+"/"+annee;
 
+// initialiser le tableau scores à vide s'il n'existe pas
 let scores = JSON.parse(localStorage.getItem('score')) || [];
 
 // tableaux vides pour les couleurs et pour une ligne de couleur afin de les comparer
@@ -84,8 +85,6 @@ for (let i = 0; i < 4; i++) {
     couleur_random.push(colorBulle);
 }
 
-console.log(couleur_random);
-
 
 function getColors(color) {
     const bulleColor = document.getElementsByClassName('bulle'),
@@ -102,14 +101,18 @@ function getColors(color) {
 
     ligneColor.push(color);
 
+    // si le jeu est fini ou si les deux tableaux ne correspondent pas, le joueur a perdu
     if (nbrBulle == ordreCouleur && JSON.stringify(couleur_random) !== JSON.stringify(ligneColor)) {
         document.body.innerHTML += "<div class='win'><h1>Game Over</h1><p>Solution :</p><div class='ligne_reponse'><div style='background:"+couleur_random[0]+"' class='bulle'></div><div style='background:"+couleur_random[1]+"' class='bulle'></div><div style='background:"+couleur_random[2]+"' class='bulle'></div><div style='background:"+couleur_random[3]+"' class='bulle'></div></div><img class='gif' src='./assets/loose.gif'><a onclick='reload()'>Rejouer</a></div>";
         tabScore(date_score, "--", laDifficulte, "Perdu");
     }
 
+    // à chaque fin de ligne, donc dès qu'il y a 4 bulles remplies
     if (ligneFini == 4) {
+        // si les deux tableaux correspondent -> gagné
         if (JSON.stringify(couleur_random) === JSON.stringify(ligneColor)) {
             document.body.innerHTML += "<div class='win'><h1>Félicitations !!</h1><img class='gif' src='./assets/win.gif'><a onclick='reload()'>Rejouer</a></div>";
+            // permet de faire le tableau des scores
             tabScore(date_score, nbEssais+1, laDifficulte, "Gagné");
         }
         else {
@@ -120,25 +123,31 @@ function getColors(color) {
                   // vérifier pour des places correctes
                   for(let j = 0; j < 4; j++){
                       if(ligneColor[j]===couleur_random[j]) {
+                        // mettre en vert si une couleur est bien placée
                           toast[i].getElementsByClassName('bulle_indice')[j].style.backgroundColor = 'green';
+                        // sinon blanc
                       } else {
                           toast[i].getElementsByClassName('bulle_indice')[j].style.backgroundColor = 'white';
                       }
                   }
-              
+                  // ajoute une classe qui permet d'afficher les indices sur le côté de la ligne
                   toast[i].classList.add('score_on');
                 } 
             }
 
             toast[ordreToast].classList.add('score_on');
+            // pouvoir passer aux indices de la ligne suivante
             ordreToast++
+            // définir à 0 la ligne pour pouvoir revérifier à chaque fin de ligne (=4)
             ligneFini = 0;
+            // vider le tableau des propositions du joueur
             ligneColor.splice(color);
         }
         nbEssais++;
     }
 }
 
+// permet de mettre dans un tableau toutes les infos pour le tableau des scores
 function tabScore(date_score, nbEssais, laDifficulte, statut) {
     scores.push({
         date_score: date_score,
@@ -146,10 +155,11 @@ function tabScore(date_score, nbEssais, laDifficulte, statut) {
         laDifficulte : laDifficulte,
         statut: statut
     });
+    // le stocker en local sur le navigateur
     localStorage.setItem("score", JSON.stringify(scores));
-    console.log(scores);
 }
 
+// permet d'insérer le tableau dans la page du tableau des scores
 function insertScore() {
     const tableau = document.getElementById('tabScore');
     
@@ -158,6 +168,7 @@ function insertScore() {
     }
 }
 
+// pour recharger la page, elle intervient sur les boutons "rejouer"
 function reload() {
     location.reload();
 }
